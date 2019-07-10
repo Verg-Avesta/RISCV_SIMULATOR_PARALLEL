a00522fb7f711deca67d02316bde556550d6400d
Chang Liu <50190610+Verg-Avesta@users.noreply.github.com>
2019-07-11 00:03:45 +0800

    Update RISCV.hpp

--- RISCV.hpp
+++ RISCV.hpp
@@ -14,5 +14,8 @@ bool PClock=0;//PC是否被锁起来
 int saber=0;//缓存区，EX-result
 int avalon=0;//缓存区，MEM-result
 int loadlock=0;//标记是不是load以及load进行到的程度
+int jump=0,notjump=0;//跳和不跳分别的次数
+double success=0,total=0;//预测成功的次数以及预测的总数；
+int jumpflag=0,errorflag=0;//预测之后是否进行跳跃以及是否预测错误
 ///都是operator所需要用到的东西
 #endif //RISCV_BRANCH_PREDICTION_RISCV_H
